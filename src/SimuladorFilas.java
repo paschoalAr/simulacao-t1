250953902761f2b7eaa5d7290af6db11f7cf11fd
paschoalAr <arthur.paschoal@edu.pucrs.br>
2026-09-12 13:09:37 -0300

    Ajustar imports

--- src/SimuladorFilas.java
+++ src/SimuladorFilas.java
@@ -1,5 +1,10 @@
 import java.math.BigInteger;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class SimuladorFilas {
 
